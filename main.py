f7b70955fdf8a5979d9cc655a9cb9f9801590f62
Nithichai Thepmong <rockon23146@gmail.com>
2018-10-25 13:54:51 +0700

    make it better

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import zeep
 
-# client = Client('https://aircon-select1-server.herokuapp.com/server.php?wsdl')
-# result = client.service.post_aircon('81-987,31.5,40.56')
-# print(result)
-
 client = zeep.Client('https://aircon-select1-server.herokuapp.com/server.php?wsdl')
-result = client.service.get_personal_data()
-print(result)
+
+def post_aircon():
+  result = client.service.post_aircon('81-987,31.5,40.56')
+  print(result)
+
+def show_personal():
+  result = client.service.get_personal_data()
+  print(result)
+
+show_personal()
